1b605a4c96e865c429ad9f853a83f38566c1ff00
barry <barry91205@gmail.com>
2023-10-30 20:45:29 +0800

    chat

---
+++ public/init-firebase.js
@@ -0,0 +1,12 @@
+// TODO: Replace the following with your app's Firebase project configuration
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDmMxhiJHnPuEkGR9KwtJQv_2Nr9DJHLz4U",
+    authDomain: "chatroom-b5b4c.firebaseapp.com",
+    databaseURL: "https://chatroom-b5b4c-default-rtdb.firebaseio.com/",
+    projectId: "chatroom-b5b4c",
+    storageBucket: "gs://chatroom-b5b4c.appspot.com/",
+};
+  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
